8ab09886eb89998fa81fee2fefffdbb4700b5e64
Serabass <is_everything@mail.ru>
2016-01-23 12:29:44 +0600

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,42 @@
-/**
- * Created by Serabass on 23.01.2016.
- */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jade = require('jade'),
+    gulpJade = require('gulp-jade'),
+    log = require('gulp-log'),
+    autoprefixer = require('gulp-autoprefixer')
+;
+
+gulp.task('styles', function () {
+    gulp.src('/static/css')
+        .pipe(sass({ onError: function (e) { console.log(e); } }))
+        .pipe(autoprefixer("last 2 versions", "> 1%", "ie 8"))
+        .pipe(gulp.dest('.'))
+    ;
+});
+
+gulp.task('jade', function () {
+    gulp.src('application/views/**/*.jade')
+        .pipe(log())
+        .pipe(gulpJade({
+            jade: jade,
+            pretty: true
+        }))
+        //.pipe(jade())
+        .pipe(gulp.dest('application/views/'))
+    ;
+});
+
+gulp.task("ts:app", function () {
+    var appConfig = {
+        "module": "umd",
+        target: "es5",
+        noImplicitAny: true,
+        experimentalDecorators: true,
+        sourceMap: true,
+        out: "app.compiled.js"
+    };
+
+    return gulp.src(['/static/js/**/*.ts', 'typings/tsd.d.ts'])
+        .pipe(plugins.tsc(appConfig))
+        .pipe(gulp.dest("public/app/"));
+});
