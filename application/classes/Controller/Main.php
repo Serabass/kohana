ae42252f11573bb3b2de6b957114f9eb7e8d31dc
Serabass <is_everything@mail.ru>
2016-01-23 20:56:31 +0600

    Controller_Main

--- application/classes/Controller/Main.php
+++ application/classes/Controller/Main.php
@@ -4,6 +4,8 @@ class Controller_Main extends Controller {
 
 	public function action_index()
 	{
+		$user = ORM::factory('User', 1);
+		// var_dump($user);
 		$this->response->body(View::factory('general'));
 	}
 
