bf76a1c5c66165cc401cb18b9852591f6a4963e0
Serabass <is_everything@mail.ru>
2016-01-23 22:28:20 +0600

    Garbage

---
+++ application/config/vk.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+
+	'VK_API_PATH'		=> 'http://api.vkontakte.ru/api.php',
+	'VK_API_ID'		=> '3037883',
+	'VK_API_PASSWORD'	=> '123123',
+	'VK_API_SECRET'		=> 'letmeinnow',
+
+	'VK_API_FILEDS'		=> array(
+		'photo',
+		'nickname',
+		'first_name',
+		'last_name',
+		'city',
+		'sex',
+		'bdate',
+		'country',
+		'photo_medium',
+		'photo_big',
+		'has_mobile',
+		'rate',
+		'contacts',
+		'education'
+	),
+
+	'VK_SESSION_KEY'	=> 'vk',
+);
